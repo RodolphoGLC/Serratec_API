bfec8e67488e96f58d19adfe6534150c9be16da1
RodolphoGLC <rodolpho.geouerj20@gmail.com>
2023-05-20 09:05:26 -0300

    Update Biblioteca - Swagger

--- Aulas/biblioteca/src/main/java/com/residencia/biblioteca/BibliotecaApplication.java
+++ Aulas/biblioteca/src/main/java/com/residencia/biblioteca/BibliotecaApplication.java
@@ -2,12 +2,13 @@ package com.residencia.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
-
 }
